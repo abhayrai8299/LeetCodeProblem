abd7490dd442e640dfef8c432adfd0101e41cc1e
abhayrai8299 <abhay.rai8299@gmail.com>
2025-09-05 23:08:00 +0530

    initial commit

---
+++ RotateByKUsingBruteForce.js
@@ -0,0 +1,14 @@
+let num=[1,2,3,4,5,6,7];
+let k=3;
+let nlen=num.length;
+while(k>0){
+    let temp=num[nlen-1];
+    for(let i=nlen-1;i>0;--i)
+    {
+        num[i]=num[i-1];
+    }
+    num[0]=temp;
+    --k;
+}
+
+console.log(num);
